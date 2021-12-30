a0af6a4140b3205973f06ac1989bf04daef2909c
Cong-1998 <92134755+Cong-1998@users.noreply.github.com>
2021-12-30 16:47:18 +0800

    Add files via upload

---
+++ Sentiment/sentiment.py
@@ -0,0 +1,26 @@
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+def en_sentiment(text):
+    analyzer = SentimentIntensityAnalyzer()
+    sent = analyzer.polarity_scores(text)
+    return sent['compund']
+    
+def detect_sentiment(df, malaya):
+    
+    # malay sentiment analysis
+    model = malaya.sentiment.multinomial()
+    clean = df['clean'].values.tolist()
+    ms_sen = model.predict(clean)
+    df = df.assign(Sentiment = ms_sen)
+
+    # english sentiment analysis
+    df.loc[df['Language'] == "en", 'Score'] = df['clean'].apply(en_sentiment)
+    df.loc[df['Score']>=0.05, 'Sentiment']='positive'
+    df.loc[(df['Score']<0.05) & (df['Score']>-0.05), 'Sentiment']='neutral'
+    df.loc[df['Score']<-0.05, 'Sentiment']='negative'
+
+    # remove unwanted coulmns
+    df = df.drop(['Score'], axis = 1)
+
+    return df
+
