ad78feb05b2a67e6eebf91527c6cbf177f0fb378
Cong-1998 <92134755+Cong-1998@users.noreply.github.com>
2021-12-30 16:45:37 +0800

    Add files via upload

---
+++ Emotion/emotion.py
@@ -0,0 +1,26 @@
+import bz2
+import pickle
+import _pickle as cPickle
+
+def en_emotion(text):
+    with bz2.BZ2File('Emotion/en_emotion.pbz2', 'rb') as training_model:
+        en_model = pickle.load(training_model)
+
+    label = en_model.predict(text)
+    return label
+    
+def detect_emotion(df, malaya):
+
+    # malay emotion analysis
+    ms_model = malaya.emotion.transformer(model = 'albert')
+    clean = df['clean'].values.tolist()
+    ms_emo = ms_model.predict(clean)
+    df = df.assign(Emotion = ms_sen)
+
+    # english emotion analysis
+    df.loc[df['Language'] == "en", 'Emotion'] = df['clean'].apply(en_emotion)
+
+    # remove unwanted coulmns
+    df = df.drop(['Language', 'clean'], axis = 1)
+
+    return df
